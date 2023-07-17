94c0958b67cf004a4d2214393a1acb6e4c1f64cd
RamilA123 <ramilna@code.edu.az>
2023-07-17 20:31:35 +0400

    finish home page slider and category part

--- Frontend - GroceryMarket/assets/js/home.js
+++ Frontend - GroceryMarket/assets/js/home.js
@@ -1,6 +1,6 @@
 // HEADER-STAR
 
-let flags = document.querySelectorAll(".flag");
+let flags = document.querySelectorAll(".country-flag");
 
 for (const flag of flags) {
 
@@ -53,5 +53,25 @@ $(document).ready(function(){
     });
     
   });
+
+const targetDate = new Date("2023-09-17 23:59:59").getTime();
+
+const countdownInterval = setInterval(function() {
+
+const currentDate = new Date().getTime();
+
+const remainingTime = targetDate - currentDate;
+
+const numberOfDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+const numberOfHours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const numberOfMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+const numberOfSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerText = numberOfDays;
+document.getElementById("hours").innerText = numberOfHours;
+document.getElementById("minutes").innerText = numberOfMinutes;
+document.getElementById("seconds").innerText = numberOfSeconds;
+
+}, 1000);
 
 // MAIN-END
