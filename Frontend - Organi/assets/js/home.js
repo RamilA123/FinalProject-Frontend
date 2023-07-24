29d5cdfcec2d5685064df15b1e2631f37d3bc602
RamilA123 <ramilna@code.edu.az>
2023-07-24 10:31:06 +0400

    finish about page

--- Frontend - Organi/assets/js/home.js
+++ Frontend - Organi/assets/js/home.js
@@ -1,77 +1,103 @@
 // FLAG
-let flags = document.querySelectorAll(".country-flag");
+let flagHeader = document.querySelector(".flag-header");
+
+let flags = document.querySelectorAll(".flag-item");
+
+flagHeader.addEventListener("click",function(){
+    if (this.nextElementSibling.classList.contains("invisible")) {
+
+        this.nextElementSibling.classList.remove("invisible");
+        this.nextElementSibling.style.cssText = `
+        height: 78px;
+        `
+    }
+    else{
+        this.nextElementSibling.classList.add("invisible");
+        this.nextElementSibling.style.cssText = `
+        height: 0;
+        `
+    }
+})
 
 for (const flag of flags) {
 
-    flag.addEventListener("click",function(){
+    flag.addEventListener("click", function () {
 
-    let flagText =  this.closest(".flags").firstElementChild.firstElementChild.firstElementChild.innerText
+        let flagText = this.closest(".flags").firstElementChild.firstElementChild.firstElementChild.innerText
 
-    let flagImage = this.closest(".flags").firstElementChild.firstElementChild.children[1].firstElementChild.getAttribute("src");
+        let flagImage = this.closest(".flags").firstElementChild.firstElementChild.children[1].firstElementChild.getAttribute("src");
 
-    this.closest(".flags").firstElementChild.firstElementChild.firstElementChild.innerText = this.firstElementChild.innerText;
+        this.closest(".flags").firstElementChild.firstElementChild.firstElementChild.innerText = this.firstElementChild.innerText;
 
-    this.closest(".flags").firstElementChild.firstElementChild.children[1].firstElementChild.setAttribute("src",this.lastElementChild.firstElementChild.getAttribute("src"));
+        this.closest(".flags").firstElementChild.firstElementChild.children[1].firstElementChild.setAttribute("src", this.lastElementChild.firstElementChild.getAttribute("src"));
 
-    this.firstElementChild.innerText = flagText;
+        this.firstElementChild.innerText = flagText;
 
-    this.lastElementChild.firstElementChild.setAttribute("src",flagImage);
+        this.lastElementChild.firstElementChild.setAttribute("src", flagImage);
 
+        if (!this.parentNode.classList.contains("invisible")) {
+
+            this.parentNode.classList.add("invisible");
+            this.parentNode.style.cssText = `
+            height: 0;
+            `
+        }
     })
 }
 // FLAG
 
 
 
+
 // OWL-CAROUSEL
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.loop').owlCarousel({
         center: true,
-        items:2,
-        loop:true,
-        margin:10,
-        responsive:{
-            600:{
-                items:4
+        items: 2,
+        loop: true,
+        margin: 10,
+        responsive: {
+            600: {
+                items: 4
             }
         }
     });
-    
+
     $('.nonloop').owlCarousel({
         center: true,
-        items:2,
-        loop:false,
-        margin:10,
-        responsive:{
-            600:{
-                items:4
+        items: 2,
+        loop: false,
+        margin: 10,
+        responsive: {
+            600: {
+                items: 4
             }
         }
     });
-    
-  });
+
+});
 // OWL-CAROUSEL
 
 
 //COUNTDOWN
 const targetDate = new Date("2023-07-30 23:59:59").getTime();
 
-const countdownInterval = setInterval(function() {
+const countdownInterval = setInterval(function () {
 
-const currentDate = new Date().getTime();
+    const currentDate = new Date().getTime();
 
-const remainingTime = targetDate - currentDate;
+    const remainingTime = targetDate - currentDate;
 
-const numberOfDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-const numberOfHours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-const numberOfMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-const numberOfSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+    const numberOfDays = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const numberOfHours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const numberOfMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    const numberOfSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-document.getElementById("days").innerText = numberOfDays;
-document.getElementById("hours").innerText = numberOfHours;
-document.getElementById("minutes").innerText = numberOfMinutes;
-document.getElementById("seconds").innerText = numberOfSeconds;
+    document.getElementById("days").innerText = numberOfDays;
+    document.getElementById("hours").innerText = numberOfHours;
+    document.getElementById("minutes").innerText = numberOfMinutes;
+    document.getElementById("seconds").innerText = numberOfSeconds;
 
 }, 1000);
 //COUNTDOWN
@@ -81,8 +107,8 @@ document.getElementById("seconds").innerText = numberOfSeconds;
 //UP-ARROW
 let upArrow = document.querySelector(".up-arrow");
 
-upArrow.addEventListener("click",function(){
-    window.scrollTo(0,0);
+upArrow.addEventListener("click", function () {
+    window.scrollTo(0, 0);
 })
 //UP-ARROW
 
